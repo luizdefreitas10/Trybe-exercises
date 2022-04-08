6ebd00b3cdca90c5e34680b6cff208eed0751f16
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-04-07 22:16:50 -0300

    adiciona oputros exercicios JavaScript

---
+++ fundamentos/bloco4-intro-javascript-logica/aula2/exemplos.js
@@ -0,0 +1,11 @@
+// let numeros = [1, 65, 789]; 
+
+// for (let corico of numeros) {
+//     console.log(corico)
+// }
+
+let array = ["vemelho", "luizo", 789, "james"]; 
+
+for (let keeper of array) {
+    console.log(keeper);
+}
