e38a939dd1ef2621b77b1075d3bd290af79711c7
Luiz de Freitase <luizdefreitas10@gmail.com>
2022-05-01 12:10:15 -0300

    adicionando exercicios webstorage

---
+++ fundamentos/bloco5-dom-seletores/javascript-webstorage/exercicios-webstorage/main.js
@@ -0,0 +1,13 @@
+window.onload = pageOk();
+
+function pageOk() {
+    alert("Página carregada");
+}
+
+function recebeP() {
+    let paragrafo = document.querySelector(".pClass");
+    paragrafo.inneHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie, augue quis sagittis hendrerit, purus metus porttitor sem, quis volutpat sapien felis eu quam. Mauris lobortis libero sed sem consequat, sit amet porta ipsum pharetra. Donec et sapien id dolor scelerisque lobortis. Cras eget suscipit tellus. Nulla aliquam risus ut lectus volutpat, ac egestas velit vehicula. Nulla consectetur erat quis lacinia efficitur. Donec eleifend posuere mauris, non interdum ex consectetur vel. Sed posuere, ante id ornare vulputate, orci metus pellentesque leo, id accumsan orci elit in sapien. Proin quis leo sit amet odio laoreet ullamcorper sed sed massa."
+}
+
+function recebeP ();
+
